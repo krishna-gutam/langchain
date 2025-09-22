3310b4016b8ae1d2790131789f3e8d5338af1983
krishnag8950 <krishnag8950@gmail.com>
2025-09-22 06:30:36 +0000

    Updated project files

--- prompt_ui.py
+++ prompt_ui.py
@@ -1,30 +1,10 @@
-from pyngrok import ngrok
-import os
-
-# Terminate open tunnels if exist
-ngrok.kill()
-
-# Setting an auth token allows us to open multiple tunnels at the same time
-# and gives you longer tunnel lifetimes.
-# Get your authtoken from https://dashboard.ngrok.com/get-started/your-authtoken
-NGROK_AUTH_TOKEN = os.environ.get("NGROK_AUTH_TOKEN")
-if NGROK_AUTH_TOKEN:
-    ngrok.set_auth_token(NGROK_AUTH_TOKEN)
-
-# Open a http tunnel on the default port 8501
-public_url = ngrok.connect(8501)
-print("=======================================================================================")
-print(f"Your Streamlit app is live! You can view it at: {public_url}")
-print("=======================================================================================")
-
-
-from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_openai import ChatOpenAI
 from dotenv import load_dotenv
 import streamlit as st
 from langchain_core.prompts import PromptTemplate,load_prompt
 
 load_dotenv()
-model = ChatGoogleGenerativeAI(model="gemini-1.5-flash")
+model = ChatOpenAI()
 
 st.header('Reasearch Tool')
 
